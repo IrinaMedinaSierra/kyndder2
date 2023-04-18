9b4e91eabe8fb30011688e827956f6c68fb8637b
IrinaMedinaSierra <irina@ceatformacion.com>
2023-04-18 14:31:31 +0200

    TERCERA version

--- src/main/java/com/cursoceat/controller/Controller.java
+++ src/main/java/com/cursoceat/controller/Controller.java
@@ -7,28 +7,61 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.cursoceat.modell.Nino;
+import com.cursoceat.modell.Tutores;
+import com.oracle.wls.shaded.org.apache.xml.utils.SystemIDResolver;
+
 /**
  * Servlet implementation class Controller
  */
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String nombreString=request.getParameter("nombre");
-		if (nombreString.isEmpty()) {
+		String nombreNino=request.getParameter("nombre");
+		String apellidosNino=request.getParameter("apellidos");
+		String fNacimiento=request.getParameter("fechaNacimiento");
+		String direccionN=request.getParameter("direccion");
+		String poblacionN=request.getParameter("poblacion");
+		String cpN=request.getParameter("cp");
+		String alergia=request.getParameter("alergias");
+		String alergiaAli=request.getParameter("AlergiaAlim");
+		String intolerancia=request.getParameter("intolerancias");
+		String medicacion=request.getParameter("medicacion");
+		String observaciones=request.getParameter("observaciones");
+		String nombreTutor1=request.getParameter("nombreApellidoT1");
+		String dniT1=request.getParameter("dniT1");
+		String profesionT1=request.getParameter("profesionT1");
+		//Ejemplo de un casting a entero
+		//int telefono=Integer.parseInt(request.getParameter("telefonoT1"));
+		String telefonoT1=request.getParameter("telefonoT1");
+		String emailT1=request.getParameter("emailT1");
+		String parentesco=request.getParameter("parentesco");
+		
+		
+		// leer todos los datos del formulario
+		//crear mis objetos -> clases
+		
+		
+		if (nombreNino.isEmpty() || apellidosNino.isEmpty() || fNacimiento.isEmpty() || direccionN.isEmpty() 
+				|| poblacionN.isEmpty() || cpN.isEmpty() || nombreTutor1.isEmpty() ||
+				dniT1.isEmpty() || telefonoT1.isEmpty() || parentesco.isEmpty()) {
+			String error="Los campos requeridos son obligatorios";
+			request.setAttribute("error", error); //no se envia hasta que se despache
+			request.getRequestDispatcher("index.jsp").forward(request, response);//no se muestra si tengo quien lo muestre
 			
+		}else {		
+			request.getRequestDispatcher("confirmar.jsp").forward(request, response);
+			Nino miNino=new Nino(nombreNino, apellidosNino, direccionN, poblacionN, cpN, fNacimiento, 
+					alergia, alergiaAli, intolerancia, medicacion, observaciones);
+			int idNino=miNino.getIdNino();
+			Tutores elTutor=new Tutores(idNino,nombreTutor1,dniT1,telefonoT1,profesionT1,emailT1,parentesco);
+			System.out.println(elTutor.toString());
+			//
 		}
 	}
 
--- src/main/java/com/cursoceat/modell/DatosGenerales.java
+++ src/main/java/com/cursoceat/modell/DatosGenerales.java
@@ -2,4 +2,45 @@ package com.cursoceat.modell;
 
 public class DatosGenerales {
 
+	protected int idNino;
+	protected String nombreApellidos;
+	protected String dni;
+	protected String telefono;
+	public DatosGenerales(int idNino, String nombreApellidos, String dni, String telefono) {
+		this.idNino = idNino;
+		this.nombreApellidos = nombreApellidos;
+		this.dni = dni;
+		this.telefono = telefono;
+	}
+	public DatosGenerales() {
+	}
+	public int getIdNino() {
+		return idNino;
+	}
+	public String getNombreApellidos() {
+		return nombreApellidos;
+	}
+	public void setNombreApellidos(String nombreApellidos) {
+		this.nombreApellidos = nombreApellidos;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	@Override
+	public String toString() {
+		return "DatosGenerales [idNino=" + idNino + ", nombreApellidos=" + nombreApellidos + ", dni=" + dni
+				+ ", telefono=" + telefono + "]";
+	}
+	
+	
+	
 }
